b9cba885001d13f5371ed0e531a05fbd62beda7f
Bill St. Clair <billstclair@gmail.com>
2024-10-15 13:03:09 -0400

    Arbitrage.elm

---
+++ src/AutoCrypTrage/Arbitrage.elm
@@ -0,0 +1,19 @@
+---------------------------------------------------------------------
+--
+-- Arbitrage.elm
+-- Calculate Arbitrage
+-- Copyright (c) 2024 Bill St. Clair <billstclair@gmail.com>
+-- Some rights reserved.
+-- Distributed under the MIT License
+-- See LICENSE
+--
+----------------------------------------------------------------------
+
+
+module AutoCrypTrage.Arbitrage exposing (findArbitrage)
+
+
+findArbitrage : List Trade -> NextTrade -> ( List Trade, NextTrade )
+findArbitrage trades nextTrade =
+    -- TODO
+    ( trades, nextTrade )
